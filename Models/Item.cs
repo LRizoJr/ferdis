8e9788f809526b27d637612cf4f6f1c0d5eee706
LRizoJr <luis_rizo@ultimatesoftware.com>
2017-03-22 07:54:23 -0400

    Added PostgreSQL EF dependency and basic Item and ItemType models to the project

---
+++ Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ferdis.Models
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public ItemType ItemType { get; set; }    
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int QtyAvailable { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
---
+++ Models/ItemType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ferdis.Models
+{
+    public class ItemType
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
